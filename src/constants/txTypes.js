de2ea1d1af9ecd9f2d46c8cff7e991aa4a035d07
Jianjiang Wang <j.wang@hashgard.com>
2019-04-19 18:00:30 +0800

    add new tx types

--- src/constants/txTypes.js
+++ src/constants/txTypes.js
@@ -1,21 +1,42 @@
 export const txTypes = {
-  // transfer
+  // bank
   send: 'Send',
 
-  // delegate to validator
+  // staking
   delegate: 'Delegate',
   begin_redelegate: 'Redelegate',
   begin_unbonding: 'Unbond',
   create_validator: 'Create Validator',
-  // edit_validator: 'Edit Validator',
+  edit_validator: 'Edit Validator',   // new
+  
+  // slashing
   unjail: 'Unjail',
-  // set_withdraw_address: 'Set Withdraw Address',
-  // withdraw_delegation_rewards_all: 'Withdraw Delegation Rewards All',
-  // withdraw_delegation_reward: 'Withdraw Delegation Reward',
-  // withdraw_validator_rewards_all: 'Withdraw Validator Rewards All',
 
-  // // proposal & vote
+  // distribution
+  set_withdraw_address: 'Set Withdraw Address',   // new
+  withdraw_delegator_reward: 'Withdraw Delegation Reward',  // new
+  withdraw_validator_rewards_all: 'Withdraw Validator Rewards All', // new
+
+  // gov
   submit_proposal: 'Submit Proposal',
   deposit: 'Deposit',
-  vote: 'Vote'
+  vote: 'Vote',
+
+  // exchange
+  create_order: 'Create Order', // new
+  take_order: 'Take Order', // new
+  withdrawal_order: 'Withdrawal Order', // new
+
+  // issue
+  issue: 'Issue', // new
+  issue_mint: 'Issue Mint', // new
+  issue_burn: 'Issue Burn', // new
+  issue_burn_from: 'Issue Burn From', // new
+  issue_burn_any: 'Issue Burn Any', // new
+  issue_finish_minting: 'Issue Finish Minting', // new
+  issue_description: 'Issue Description', // new
+  issue_transfer_ownership: 'Issue Transfer Ownership', // new
+  issue_burn_off: 'Issue Burn Off', // new
+  issue_burn_from_off: 'Issue Burn From Off', // new
+  issue_burn_any_off: 'Issue Burn Any Off', // new
 };
