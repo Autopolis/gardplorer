2d2c1810735c7a6fa4f14f75ed490c106a60191f
Shawn Yang <s.yang@hashgard.com>
2019-04-19 18:11:42 +0800

    update proposal page deposit amount

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,7 +1,8 @@
 import moment from 'dayjs';
+import { isEmpty } from 'lodash';
 
 export const upper = s => s.toUpperCase();
 
 export const formatTime = time => moment(time).format('YYYY-MM-DD HH:mm:ss');
 
-export const amount = ls => ls.map(i => `${i.amount} ${i.denom}`).join(', ');
+export const amount = ls => (isEmpty(ls) ? '0' : ls.map(i => `${i.amount} ${i.denom}`).join(', '));
