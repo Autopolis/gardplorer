41926fa09b9367e4def724c126b35ae363cd5974
Shawn Yang <s.yang@hashgard.com>
2019-04-26 16:09:37 +0800

    update NotFound page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="not-found-container">
-    <p>Sorry, This page is not found.</p>
-  </div> 
+    <Card title="Not Found">
+      <div class="content">
+        <p>Sorry, This page is not found.</p>
+        <p>Return to <router-link to="/">HomePage</router-link>.</p>
+      </div>
+    </Card>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .not-found-container {
-  margin-top: 200px;
-  width: 100%;
-  height: 100%;
-  font-size: 32px;
+  font-size: 24px;
   text-align: center;
+  color: rgba(255, 255, 255, 0.8);
+
+  .content {
+    margin: 64px 0;
+  }
 }
 </style>
 
